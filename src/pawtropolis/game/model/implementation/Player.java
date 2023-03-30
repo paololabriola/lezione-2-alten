e0a619e3dc1489fc460aea9de90da0d5c1b00c09
paololabriola <paololabriola02@gmail.com>
2023-03-30 12:53:49 +0200

    Add Concrete Class Player extending Entity with its constructor

---
+++ src/pawtropolis/game/model/implementation/Player.java
@@ -0,0 +1,12 @@
+package pawtropolis.game.model.implementation;
+
+import pawtropolis.game.model.abstractation.Entity;
+
+public class Player extends Entity {
+    public Player(String name, int healthPoints) {
+
+        super(name, healthPoints);
+
+    }
+
+}
