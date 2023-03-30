966c6b844b8f28689826856000c539db60ee2075
paololabriola <paololabriola02@gmail.com>
2023-03-30 12:49:21 +0200

    Add Abstract Class Entity with its attributes, constructor and getters and setters

---
+++ src/pawtropolis/game/model/abstractation/Entity.java
@@ -0,0 +1,34 @@
+package pawtropolis.game.model.abstractation;
+
+public abstract class Entity {
+
+    //Attributes
+    private String name;
+    private int healthPoints;
+
+    //Constructor
+    public Entity(String name, int healthPoints) {
+
+        this.name = name;
+        this.healthPoints = healthPoints;
+
+    }
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+}
