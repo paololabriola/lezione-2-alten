a0ee9c981554ecbe7f605ce8de23063aacba2151
paololabriola <paololabriola02@gmail.com>
2023-03-30 11:46:45 +0200

    Add concrete class Tiger extending Tailed with its constructor

---
+++ src/pawtropolis/animals/model/implementation/Tiger.java
@@ -0,0 +1,14 @@
+package pawtropolis.animals.model.implementation;
+
+import pawtropolis.animals.model.abstractation.Tailed;
+
+public class Tiger extends Tailed {
+
+    //Class constructor
+    public Tiger(String name, String favouriteFood, int age, double weight, double height, double tailLength) {
+
+        super(name, favouriteFood, age, weight, height, tailLength);
+
+    }
+
+}
