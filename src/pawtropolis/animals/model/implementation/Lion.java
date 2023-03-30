15dab731816dbf7e114731837b1e727b5cc898bd
paololabriola <paololabriola02@gmail.com>
2023-03-30 11:44:40 +0200

    Add concrete class Lion extending Tailed with its constructor

---
+++ src/pawtropolis/animals/model/implementation/Lion.java
@@ -0,0 +1,14 @@
+package pawtropolis.animals.model.implementation;
+
+import pawtropolis.animals.model.abstractation.Tailed;
+
+public class Lion extends Tailed {
+
+    //Class constructor
+    public Lion(String name, String favouriteFood, int age, double weight, double height, double tailLenght) {
+
+        super(name, favouriteFood, age, weight, height, tailLenght);
+
+    }
+
+}
