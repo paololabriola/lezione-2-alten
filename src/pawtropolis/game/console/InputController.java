88b58da4d800c30d1fb94afe43a7761100eaa267
paololabriola <paololabriola02@gmail.com>
2023-03-30 16:27:38 +0200

    Add Class InputController with readString method to read an input String value

---
+++ src/pawtropolis/game/console/InputController.java
@@ -0,0 +1,26 @@
+package pawtropolis.game.console;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputController {
+
+    public static String readString() {
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader inputReader = new BufferedReader(input);
+        try {
+
+            return inputReader.readLine();
+
+        } catch (IOException e) {
+
+            System.err.println("Error while reading user input");
+            return "";
+
+        }
+
+    }
+
+}
