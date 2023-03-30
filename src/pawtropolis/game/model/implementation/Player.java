79d02793b94703ccb668b4ea39adc6ea9e56699e
paololabriola <paololabriola02@gmail.com>
2023-03-30 17:39:22 +0200

    Add a bag to Player's attributes, with its getter and setter. Defining a constant attribute for default bag capacity on creation of a new Player

--- src/pawtropolis/game/model/implementation/Player.java
+++ src/pawtropolis/game/model/implementation/Player.java
@@ -3,10 +3,27 @@ package pawtropolis.game.model.implementation;
 import pawtropolis.game.model.abstractation.Entity;
 
 public class Player extends Entity {
+
+    static final int DEFAULT_BAG_CAPACITY = 10;
+
+    //Attributes
+    private Bag bag;
+
+    //Constructor
     public Player(String name, int healthPoints) {
 
         super(name, healthPoints);
+        this.bag = new Bag(DEFAULT_BAG_CAPACITY);
 
+    }
+
+    //Getters and setters
+    public Bag getBag() {
+        return bag;
+    }
+
+    public void setBag(Bag bag) {
+        this.bag = bag;
     }
 
 }
