f2d5f716b3ca24d4f4bdb96ff11c94056c98779f
paololabriola <paololabriola02@gmail.com>
2023-03-30 15:07:51 +0200

    Add Concrete Class Room with its attributes, constructor and getters and setters

---
+++ src/pawtropolis/game/map/domain/Room.java
@@ -0,0 +1,50 @@
+package pawtropolis.game.map.domain;
+
+import pawtropolis.game.model.implementation.Item;
+import pawtropolis.zoo.model.abstractation.Animal;
+
+import java.util.Collection;
+
+public class Room {
+
+    //Attributes
+    private String name;
+    private Collection<Item> items;
+    private Collection<Animal> animals;
+
+    //Constructor
+
+    public Room(String name, Collection<Item> items, Collection<Animal> animals) {
+
+        this.name = name;
+        this.items = items;
+        this.animals = animals;
+
+    }
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Collection<Item> items) {
+        this.items = items;
+    }
+
+    public Collection<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(Collection<Animal> animals) {
+        this.animals = animals;
+    }
+
+}
