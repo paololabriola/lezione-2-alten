fe930787cecda053ba290accf3f738f6f56fabaa
paololabriola <paololabriola02@gmail.com>
2023-03-30 11:46:32 +0200

    Add concrete class Eagle extending Winged with its constructor

---
+++ src/pawtropolis/animals/model/implementation/Eagle.java
@@ -0,0 +1,14 @@
+package pawtropolis.animals.model.implementation;
+
+import pawtropolis.animals.model.abstractation.Winged;
+
+public class Eagle extends Winged {
+
+    //Class constructor
+    public Eagle(String name, String favouriteFood, int age, double weight, double height, double wingSpan) {
+
+        super(name, favouriteFood, age, weight, height, wingSpan);
+
+    }
+
+}
