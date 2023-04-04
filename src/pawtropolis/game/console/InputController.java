669a001b3d8aa92e07615f0f6c30a3e1554559a9
paololabriola <paololabriola02@gmail.com>
2023-04-04 11:10:27 +0200

    Add getCommandFromString method to InputController class to get the command from an input string

--- src/pawtropolis/game/console/InputController.java
+++ src/pawtropolis/game/console/InputController.java
@@ -3,6 +3,7 @@ package pawtropolis.game.console;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class InputController {
 
@@ -20,6 +21,29 @@ public class InputController {
             return "";
 
         }
+
+    }
+
+    public static String getCommandFromString(String input) {
+
+       char[] inputToChar = input.toCharArray();
+       char[] command = new char[inputToChar.length];
+       int count = 0;
+
+        for(char c: inputToChar) {
+
+            if(c == ' ')
+                break;
+            else {
+
+                command[count] = c;
+                count++;
+
+            }
+
+        }
+
+        return Arrays.toString(command);
 
     }
 
