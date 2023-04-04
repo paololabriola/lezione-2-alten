7a0ad29d03c4f91c93b02ad4e1723bfd2d25e200
paololabriola <paololabriola02@gmail.com>
2023-04-04 17:45:34 +0200

    Add method to class Direction getOppositeDirection for the link and unlink methods in class Room

--- src/pawtropolis/map/domain/Direction.java
+++ src/pawtropolis/map/domain/Direction.java
@@ -7,14 +7,29 @@ public enum Direction {
     EAST("east"),
     WEST("west");
 
+    //Attributes
     private final String name;
 
     private Direction(String name) {
         this.name = name;
     }
 
+    //Getters and setters
     public String getName() {
         return name;
+    }
+
+    //Other methods
+    public Direction getOppositeDirection() {
+
+        return switch (this) {
+            case NORTH -> SOUTH;
+            case SOUTH -> NORTH;
+            case EAST -> WEST;
+            case WEST -> EAST;
+            default -> throw new IllegalStateException("The following direction doesn't exist: " + this);
+        };
+
     }
 
 }
