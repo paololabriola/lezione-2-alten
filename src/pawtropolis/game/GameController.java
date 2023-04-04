389d0f2bb18acc5c70237c1cfc2ab19da15c8623
paololabriola <paololabriola02@gmail.com>
2023-04-04 18:08:09 +0200

    Add go and help command case to the switch console in class GameController

--- src/pawtropolis/game/GameController.java
+++ src/pawtropolis/game/GameController.java
@@ -3,6 +3,7 @@ package pawtropolis.game;
 import pawtropolis.game.console.InputController;
 import pawtropolis.game.model.implementation.Item;
 import pawtropolis.game.model.implementation.Player;
+import pawtropolis.map.domain.Direction;
 import pawtropolis.map.domain.Room;
 
 public class GameController {
@@ -28,7 +29,8 @@ public class GameController {
         while(!gameEnded) {
 
             String input;
-            System.out.println("Where are you going to go?");
+            System.out.println("What are you going to do?");
+            System.out.println("Type \"help\" to know the possible commands.");
             System.out.print(">");
             input = InputController.readString();
             String commandFromString = InputController.getCommandFromString(input);
@@ -38,6 +40,17 @@ public class GameController {
 
                 default:
                     System.out.println("Command \"" + commandFromString + "\" doesn't exist. Try again with an existing command.");
+                    break;
+
+                case "go":
+                    Direction direction = Direction.valueOf(commandObject.toUpperCase());
+                    Room nextRoom = currentRoom.getLinkedRoom(direction);
+                    if (nextRoom != null) {
+                        System.out.println("Moving to " + direction.getName() + " room: " + nextRoom.getName());
+                        currentRoom = nextRoom;
+                    } else {
+                        System.out.println("There is no room in that direction.");
+                    }
                     break;
 
                 case "bag":
@@ -58,6 +71,8 @@ public class GameController {
                     currentRoom.addItem(droppedItem);
                     break;
 
+                case "help":
+                    System.out.println("Commands are: go <direction>, bag, look, get <item>, drop <item>, exit.");
 
                 case "exit":
                     gameEnded = true;
