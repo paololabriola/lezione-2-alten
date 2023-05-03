b7e0dab6f7a553f41be48f15509f65cde510d818
paololabriola <paololabriola02@gmail.com>
2023-05-03 10:55:52 +0200

    refactor(Animal): implement Lombok annotation for getter and setter.

--- src/pawtropolis/zoo/model/abstractation/Animal.java
+++ src/pawtropolis/zoo/model/abstractation/Animal.java
@@ -4,21 +4,17 @@ import pawtropolis.game.model.abstractation.Entity;
 
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class Animal extends Entity {
-
-    //Class attributes
-
     private String favouriteFood;
-
     private int age;
-
     private LocalDate importDate;
-
     private double weight;
-
     private double height;
-
-    //Class constructor
     protected Animal(String name, int healthPoints, String favouriteFood, int age, double weight, double height) {
 
         super(name, healthPoints);
@@ -36,47 +32,6 @@ public abstract class Animal extends Entity {
         this.age = age;
         this.importDate = importDate;
         this.weight = weight;
-        this.height = height;
-    }
-
-    //Class methods getter and setter
-    public String getFavouriteFood() {
-        return favouriteFood;
-    }
-
-    public void setFavouriteFood(String favouriteFood) {
-        this.favouriteFood = favouriteFood;
-    }
-
-    public int getAgeOfTheSpecimen() {
-        return age;
-    }
-
-    public void setAge(int ageOfTheSpecimen) {
-        this.age = ageOfTheSpecimen;
-    }
-
-    public LocalDate getImportDate() {
-        return importDate;
-    }
-
-    public void setImportDate(LocalDate importDate) {
-        this.importDate = importDate;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
         this.height = height;
     }
 
