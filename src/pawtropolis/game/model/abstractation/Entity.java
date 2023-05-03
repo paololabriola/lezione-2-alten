bdc15c75c598081bcb0fdc67d7eeffc8c8ad8abe
paololabriola <paololabriola02@gmail.com>
2023-05-03 10:02:38 +0200

    fix(Entity): implement Lombok to generate getters, setters and all args constructor

--- src/pawtropolis/game/model/abstractation/Entity.java
+++ src/pawtropolis/game/model/abstractation/Entity.java
@@ -1,34 +1,12 @@
 package pawtropolis.game.model.abstractation;
 
-public abstract class Entity {
+import lombok.Getter;
+import lombok.Setter;
 
-    //Attributes
+@Getter
+@Setter
+@AllArgsConstructor
+public abstract class Entity {
     private String name;
     private int healthPoints;
-
-    //Constructor
-    protected Entity(String name, int healthPoints) {
-
-        this.name = name;
-        this.healthPoints = healthPoints;
-
-    }
-
-    //Getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHealthPoints() {
-        return healthPoints;
-    }
-
-    public void setHealthPoints(int healthPoints) {
-        this.healthPoints = healthPoints;
-    }
-
 }
