b4ab32b985fb3b8231fc3ad45bcedf840396ec8b
paololabriola <paololabriola02@gmail.com>
2023-05-03 10:35:15 +0200

    feat(GameController): add game controller for managing game state and dynamically proceed into the game depending on player's inputs.

--- src/pawtropolis/game/GameController.java
+++ src/pawtropolis/game/GameController.java
@@ -1,110 +1,65 @@
 package pawtropolis.game;
 
+import pawtropolis.game.command.CommandFactory;
+import pawtropolis.game.command.domain.Command;
 import pawtropolis.game.console.InputController;
-import pawtropolis.game.model.implementation.Item;
 import pawtropolis.game.model.implementation.Player;
-import pawtropolis.map.domain.Direction;
-import pawtropolis.map.domain.Room;
+import pawtropolis.map.MapController;
+import pawtropolis.map.model.Direction;
+import pawtropolis.map.model.Room;
 
 public class GameController {
 
     //Attributes
-    private final Room entry;
-    private final Player player;
+    private static GameController instance;
+    private final MapController mapController;
+    private boolean gameEnded;
+    private Player player;
 
     //Constructor
-    public GameController(Room entry, Player player) {
-
-        this.entry = entry;
-        this.player = player;
-
+    private GameController() {
+        this.mapController = new MapController();
+        this.gameEnded = false;
     }
+
+    public static GameController getInstance() {
+        if (instance == null) {
+            instance = new GameController();
+        }
+        return instance;
+    }
+
+    public Room getCurrentRoom() {
+        return mapController.getCurrentRoom();
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
 
     //Other methods
+    public boolean changeRoom(Direction direction) {
+        return mapController.changeRoom(direction);
+    }
+    public void exitGame() {
+        this.gameEnded = true;
+    }
     public void runGame() {
+        CommandFactory commandFactory = CommandFactory.getInstance();
 
-        Room currentRoom = entry;
-        boolean gameEnded = false;
+        System.out.println("Welcome to Pawtropolis OwO\n");
+        System.out.println("Insert player's name: ");
+        String playerName = InputController.readString();
+        player = new Player(playerName, 20);
 
         while(!gameEnded) {
-
-            String input;
-            System.out.println("What are you going to do?");
-            System.out.println("Type \"help\" to know the possible commands.");
+            System.out.println("What are you going to do " + playerName + "?");
             System.out.print(">");
-            input = InputController.readString();
-            String commandFromString = InputController.getCommandFromString(input);
-            String commandObject = InputController.getCommandObjectFromString(input);
 
-            switch (commandFromString) {
-
-                case "go" -> {
-
-                    if(commandObject != null) {
-
-                        try {
-
-                            Direction direction = Direction.valueOf(commandObject.toUpperCase());
-                            Room nextRoom = currentRoom.getLinkedRoom(direction);
-                            if (nextRoom != null) {
-                                System.out.println("Moving to " + direction.getName() + " room: " + nextRoom.getName());
-                                nextRoom.getRoomDescription();
-                                currentRoom = nextRoom;
-                            } else {
-                                System.out.println("There is no room in that direction.");
-                            }
-
-                        } catch (IllegalArgumentException e) {
-                            System.out.println("You must select a valid direction.");
-                        }
-
-                    }else
-                        System.out.println("You must select a direction.");
-
-                }
-
-                case "bag" -> player.getBag().showItems();
-
-                case "look" -> currentRoom.getRoomDescription();
-
-                case "get" -> {
-
-                    if(commandObject != null) {
-
-                        if(player.pickupItem(currentRoom.findItemByName(commandObject)))
-                            currentRoom.removeItem(commandObject);
-
-                    } else
-                        System.out.println("You must select an item.");
-
-                }
-
-                case "drop" -> {
-
-                    if(commandObject != null) {
-
-                        Item droppedItem = player.dropItem(commandObject);
-
-                        if(droppedItem != null)
-                            currentRoom.addItem(droppedItem);
-
-                    } else
-                        System.out.println("You must select an item.");
-
-                }
-
-                case "help" ->
-                        System.out.println("Commands are: go <direction>, bag, look, get <item>, drop <item>, exit.");
-
-                case "exit" -> gameEnded = true;
-
-                default ->
-                        System.out.println("Command \"" + commandFromString + "\" doesn't exist. Try again with an existing command.");
-
-            }
-
+            String input = InputController.readString();
+            Command command = commandFactory.makeCommandFromString(input);
+            command.run();
         }
-
     }
-
 }
